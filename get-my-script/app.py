e3917bbafa9e2474de978dc064c17dddf9ac8abe
gonzalophd <gonzalo.parada@ug.uchile.cl>
2024-06-13 18:01:26 -0400

    updated version

--- get-my-script/app.py
+++ get-my-script/app.py
@@ -18,9 +18,8 @@ def load_original_data(url):
         return None
 
 df = load_original_data(url)
-st.write(df)
 
-# Check if the 'Description' column exists
+# Check if the 'Descripcion' column exists
 if df is not None and 'Descripcion' not in df.columns:
     st.error("La columna 'Descripcion' falta en el archivo script_DB. Por favor, checa este archivo.")
 else:
@@ -28,16 +27,16 @@ else:
     model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
 
     # Precompute embeddings for existing descriptions
-    df['embeddings'] = df['Descripcion'].apply(lambda x: model.encode(x, convert_to_tensor=True))
+    df['embeddings'] = df['Descripcion'].apply(lambda x: model.encode(x))
 
     def find_best_matches(description, threshold=0.3, recommendation=False):
         start_time = time.time()
 
         # Generate embedding for the user input description
-        user_embedding = model.encode(description, convert_to_tensor=True)
+        user_embedding = model.encode(description)
 
         # Calculate cosine similarities
-        cosine_similarities = cosine_similarity(user_embedding.unsqueeze(0), list(df['embeddings']))[0]
+        cosine_similarities = cosine_similarity([user_embedding], list(df['embeddings']))[0]
 
         # Sort indices based on similarity scores in descending order
         sorted_indices = cosine_similarities.argsort()[::-1]
@@ -71,13 +70,13 @@ else:
             matches, time_taken = find_best_matches(user_description, threshold, recommendation)
 
             if not matches:
-                st.write("Sorry, no encontré nada, hora de programar.")
+                st.write("Lo siento, no encontré nada.")
             else:
                 # Display matches in a table
                 results_df = pd.DataFrame(matches, columns=["Script Name", "Factor de similitud"])
                 st.write("### Lo mejor que encontré:")
                 st.dataframe(results_df)
 
-                st.write(f"**Me tomó:** {time_taken:.4f} seconds")
+                st.write(f"**Me tomó:** {time_taken:.4f} segundos")
         else:
             st.write("Por favor escribe una descripción.")
